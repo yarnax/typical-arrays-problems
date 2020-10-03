782a2f42e4e58982528b9abf7115840e0dc4b5a2
yarnax <feniks_63@mail.ru>
2020-10-03 09:38:03 +0300

    init: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;    
+  }
+  let minimumValue = Infinity;
+  array.forEach(element => {
+    if (element < minimumValue) {
+      minimumValue = element;      
+    }
+  });
+  return minimumValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;    
+  }
+  let maximumValue = -Infinity;
+  array.forEach(element => {
+    if (element > maximumValue) {
+      maximumValue = element;      
+    }
+  });
+  return maximumValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  let sumOfArray = 0;
+  array.forEach(element => {
+    sumOfArray += element;    
+  });
+
+  return sumOfArray / array.length;  
 }
